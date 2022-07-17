98c34e8baa843b65466c3627a327458d0e49e7c7
Manukyan, Kristina <Kristina_Manukyan@epam.com>
2022-07-17 15:12:30 +0400

    Initial commit

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	
+	"io/ioutil"
+
+	
 	"log"
 	"net/http"
 	"os"
@@ -21,10 +25,43 @@ main function reads host/port from env just for an example, flavor it following 
 func Start(host string, port int) {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/name/{PARAM}", ParamGetHandler)
+	router.HandleFunc("/bad", GetBad)
+	router.HandleFunc("/data", PostData)
+	router.HandleFunc("/headers", PostHeaders)
+
+
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
 	}
+}
+func PostHeaders(w http.ResponseWriter, r *http.Request){
+	//w.WriteHeader(http.StatusOK)
+	a, _ := strconv.Atoi(r.Header.Get("a"))
+	b, _ := strconv.Atoi(r.Header.Get("b"))
+	w.Header().Set("a+b", strconv.Itoa(a+b))
+}
+
+func PostData(w http.ResponseWriter, r *http.Request){
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Print(err)
+	}
+	
+	w.Write([]byte(fmt.Sprint("I got message:\n", string(body))))
+}
+
+func GetBad(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(500)
+}
+
+func ParamGetHandler(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprint("Hello, ", vars["PARAM"])))
+
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
